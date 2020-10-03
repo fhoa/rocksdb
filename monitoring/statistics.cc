fa1096eb1543c2a37bca88416c16e1ee05bb34e1
fhoa <fhoa@itu.dk>
2020-10-03 15:19:48 +0200

    test with nullptr check

--- monitoring/statistics.cc
+++ monitoring/statistics.cc
@@ -258,7 +258,7 @@ std::shared_ptr<Statistics> CreateDBStatistics() {
 std::atomic<int> initLocks(0);
 
 void RecordTicker(uint32_t tickerType){
-  if(!!stats_){
+  if(!!stats_ && !!stats_.get()){
     std::cout << "NOT NULL" << std::endl;
     stats_.get()->recordTick(tickerType, 1);
   }
